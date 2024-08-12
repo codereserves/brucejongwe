d376315aa16276ecdc3d6d4ecfef1c88bacb70f2
bruce <brucejongwe6@gmail.com>
2024-08-13 01:17:11 +0200

    Initial upadates

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ function AppHeader() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Codereserves</Navbar.Brand>
+        <Navbar.Brand href="#home">BRUCE</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
